bebb36f4f646e820f3f508280c1206baeabfe735
willlenore <36278025+willlenore@users.noreply.github.com>
2020-04-17 20:44:26 -0400

    Updated files for bio issue

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
+
 
 # Create your models here.
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete= models.CASCADE)
+	user_bio = models.CharField(max_length=300)
 	image = models.ImageField(default='default_image.jpg', upload_to='profile_photos')
-	bio = models.TextField(max_length=280)
+	
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 def registerView(request):
 	if request.method == 'POST':
@@ -16,6 +18,25 @@ def registerView(request):
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
+
 @login_required	#user must be logged in to view this page
 def profile(request):
-    return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		user_update = UserUpdateForm(request.POST, instance=request.user)
+		profile_update = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+		if user_update.is_valid() and profile_update.is_valid():
+			profile_update.save()
+			user_update.save()
+			messages.success(request, 'Profile updated!')
+			return redirect('profile')
+
+	else:
+		user_update = UserUpdateForm(instance=request.user)
+		profile_update = ProfileUpdateForm(instance=request.user.profile)
+
+	keyVals = {
+		'user_update': user_update,
+		'profile_update': profile_update
+	}
+	return render(request,'users/profile.html',keyVals)
