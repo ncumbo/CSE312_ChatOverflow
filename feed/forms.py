5c4d6a723586de251647844d92e5c65f3b4d1f9a
ncumbo <nickcumbo42@gmail.com>
2020-05-10 19:03:24 -0400

    comment functionality fully works

--- feed/forms.py
+++ feed/forms.py
@@ -1,15 +1,7 @@
 from django import forms
 from .models import Post, Comment
 
-# class Upload(forms.ModelForm):
-#
-#     image = forms.FileField(required=False)
-#
-#     class Meta:
-#         model = Post
-#         fields = ('content', 'image')
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('content',)
+        fields = ('content','date_posted')
